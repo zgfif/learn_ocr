a2b38c00b5669dec68bcf0d9fcf62be9a6c6ae9e
zgfif <zihzag@gmail.com>
2026-03-11 15:56:16 +0200

    update test_point

--- tests/test_point.py
+++ tests/test_point.py
@@ -1,60 +1,70 @@
 from app.point import Point
 from pytest import raises
+import pytest
 
 
 
 def test_point1():
     p1 = Point(0, 0)
     p2 = Point(0, 244)
-    assert p1.close_to(p2) is False
+    assert not p1.close_to(p2)
 
 
 def test_point2():
     p1 = Point(0, 244)
     p2 = Point(27, 244)
-    assert p1.close_to(p2) is True
+    assert p1.close_to(p2)
 
 
 def test_point3():
     p1 = Point(0, 244)
     p2 = Point(27, 310)
-    assert p1.close_to(p2) is False
+    assert not p1.close_to(p2)
 
 
 def test_point4():
     p1 = Point(0, 244)
     p2 = Point(0, 244)
-    assert (p1 == p2) is True
+    assert p1 == p2
 
 
 def test_point5():
     p1 = Point(0, 244)
     p2 = Point(0, 243)
-    assert (p1 == p2) is False
+    assert p1 != p2
 
 
 def test_point6():
     p1 = Point(0, 244)
     p2 = Point(1, 244)
-    assert (p1 == p2) is False
+    assert p1 != p2
 
 
-def test_point7():
+@pytest.mark.parametrize("coordinates", [
+    ('2', 1),
+    (1, '2'),
+])
+def test_point_raising_error(coordinates):
     message: str = "Coordinates must be integers"
     with raises(TypeError) as excinfo:
-        Point('2', 1) # type: ignore[arg-type]
+        Point(*coordinates)
     assert message in str(excinfo.value)
-
-
-def test_point8():
-    message: str = "Coordinates must be integers"
-    with raises(TypeError) as excinfo:
-        Point(2, '1') # type: ignore[arg-type]
-    assert message in str(excinfo.value)
-
 
 
 def test_point9():
     p1 = Point(0, 244)
     p2 = 'abc'
-    assert (p1 == p2) is False
+    assert p1 != p2
+
+
+def test_point10():
+    p1 = Point(0, 0)
+    p2 = Point(0, Point.CLOSENESS_THRESHOLD)
+    assert p1.close_to(p2)
+
+
+def test_point11():
+    p1 = Point(0, 0)
+    p2 = Point(0, Point.CLOSENESS_THRESHOLD + 1)
+    assert not p1.close_to(p2)
+
